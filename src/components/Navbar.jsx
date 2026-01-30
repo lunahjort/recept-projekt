7f15b106ef951166177c6903ee4297d713af6d46
Luna <lunahjort@hotmail.com>
2026-01-31 00:42:16 +0100

    skapade filstrukturer inom src/, alla med tillhörande css filer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="navbar-logo">🍳 Recipe App</h2>
+      <div className="navbar-links">
+        <Link to="/" className="navbar-link">Home</Link>
+        <Link to="/favorites" className="navbar-link">Favorites</Link>
+        <Link to="/about" className="navbar-link">About</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+import RecipeCard from '../components/RecipeCard';
+import SearchBar from '../components/SearchBar';
+import './Home.css';
