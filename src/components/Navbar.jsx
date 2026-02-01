053ede5614f55548481755a29cb8314cfb751495
Luna <lunahjort@hotmail.com>
2026-02-01 01:42:22 +0100

    fixade navbar.jsx och la till navbar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2 className="navbar-logo">🍳 Recipe App</h2>
+      <h2 className="navbar-logo">The Recipe App</h2>
       <div className="navbar-links">
         <Link to="/" className="navbar-link">Home</Link>
         <Link to="/favorites" className="navbar-link">Favorites</Link>
